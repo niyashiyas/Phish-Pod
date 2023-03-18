b6e52417b1d4e28b009c8a3133269f1985972c68
Alaka A J <alaka03aj@gmail.com>
2023-03-18 21:21:45 +0530

    Results Page

---
+++ src/components/Result/Result.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from 'Navbar.js';
+
+const Result = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <h1>Results</h1>
+        <div>
+            <img src=''></img>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Result
