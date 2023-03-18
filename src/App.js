ac654a6cf3f733f4bb73f2f44a2859b30417b391
RahulAnup <rahulanup03@gmail.com>
2023-03-19 03:25:37 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Routes ,Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import Form from './components/Form';
@@ -7,13 +8,38 @@ import Glossary from './components/Glossary/Glossary';
 function App() {
   return (
     <>
-      <div>
-        <Navbar />
-        <Main />
-        <Glossary />                    
+      
+      <div className="App">
+        
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/glossary" element={
+            <>
+            <Navbar/>
+          <Glossary/></>
+            
+          } />
+        </Routes>
       </div>
+    
     </>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+   
+  );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <>
       <nav className="navbar">
         <h2 className="phishpod">phishpod</h2>
         <ul className="hg">
-          <li>Home</li>
-          <li>Glossary</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/glossary">Glossary</Link>
+          </li>    
         </ul>
       </nav>
     </>
@@ -15,3 +20,8 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
+
+
+
