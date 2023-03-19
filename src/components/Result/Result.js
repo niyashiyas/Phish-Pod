6f21d0af72fa75517c148b82505c0648a249c405
Alaka A J <alaka03aj@gmail.com>
2023-03-19 05:34:12 +0530

    added functionality for Report Now

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -10,7 +10,7 @@ const Result = (props) => {
         <div className='resultsContent'>
             <img src = './images/results-img.png'></img>
             <p>This message has been reported spam by {props.num} people!</p>
-            <button className='reportButton'>Report Now!</button>
+            <button className='reportButton'><a href='https://cybercrime.gov.in/Webform/Crime_AuthoLogin.aspx' target={"_blank"}>Report Now!</a></button>
         </div>
       </div>
     </div>
