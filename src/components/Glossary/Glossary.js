b85beb5b5130b12e07776ebe9f57333636b0a10d
Niya Shiyas <niyashiyas03@gmail.com>
2023-03-19 08:39:46 +0530

    glossary added

--- src/components/Glossary/Glossary.js
+++ src/components/Glossary/Glossary.js
@@ -4,28 +4,28 @@ import "./Glossary.css";
 export default function Glossary() {
   const [cards] = useState([
     {
-      title: "Card-1",
-      data: "Random Content",
+      title: "Phishing",
+      data: "Phishing is when a bad guy pretends to be someone else, like a teacher or a parent, and tries to trick you into giving them your personal information like your name, address, or password",
     },
     {
-      title: "Card-2",
-      data: "Random Content",
+      title: "http and https",
+      data: "HTTP is like talking on a regular phone, where anyone can listen to the conversation. HTTPS is like talking on a secret phone where only you and your friend can hear what you're saying.",
     },
     {
-      title: "Card-3",
-      data: "Random Content",
+      title: "Pharming",
+      data: "Pharming is when bad guys try to trick your computer so that when you type in a website address, like www.google.com your computer goes to a fake website instead of the real one",
     },
     {
-      title: "Card-4",
-      data: "Random Content",
+      title: "ddos",
+      data: "DDoS stands for Distributed Denial of Service, which is like a really big group of robots that try to visit a website all at the same time",
     },
     {
-      title: "Card-5",
-      data: "Random Content",
+      title: "Encryption",
+      data: "Encryption is like a secret code that keeps your messages safe from bad guys who might try to read them",
     },
     {
-      title: "Card-6",
-      data: "Random Content",
+      title: "Clickjacking",
+      data: "It's like if someone put a picture of a toy on top of a button that said Buy Now, and you accidentally clicked the button when you really just wanted to look at the toy",
     },
   ]);
   const [query, setQuery] = useState("");
