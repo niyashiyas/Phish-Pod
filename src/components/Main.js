352419b2bbc8f29db2955f0344ea0e30048824f4
Alaka A J <alaka03aj@gmail.com>
2023-03-19 11:05:19 +0530

    removed results part and fixed p tag css

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Main.css";
 import Form from "./Form";
 import Footer from "./Footer/Footer";
-import Result from "./Result/Result";
+// import Result from "./Result/Result";
 function Main() {
   return (
     <>
@@ -27,8 +27,6 @@ function Main() {
         </div>
       </div>
       <Form />
-
-      <Result num="10" />
       <Footer />
     </>
   );
