19e742d14f053b44ef138db7a569b3092aec3b03
Niya Shiyas <niyashiyas03@gmail.com>
2023-03-19 07:39:39 +0530

    main design updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Main.css";
 import Form from "./Form";
 import Footer from "./Footer/Footer";
-import Result from './Result/Result';
+import Result from "./Result/Result";
 function Main() {
   return (
     <>
@@ -10,21 +10,25 @@ function Main() {
         <div className="main-content">
           <div>
             <div>
-              <h1>HERE GOES THE TITLE</h1>
+              <h1 className="main-heading">BETTER SAFE THAN SORRY</h1>
             </div>
             <div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+              <p className="main-p">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco
+              </p>
             </div>
+            <button className="get-started">GET STARTED</button>
           </div>
 
           {/* <img src='./images/blob.png' alt='' className='blob' /> */}
           <img src="./images/mainpic.png" alt="" className="mainpic" />
         </div>
-        <button className="get-started">GET STARTED</button>
       </div>
       <Form />
-      
-      <Result num="10"/>
+
+      <Result num="10" />
       <Footer />
     </>
   );
